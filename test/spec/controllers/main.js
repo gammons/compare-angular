e07184cbd9840afd4954c8bc4791b024e316825c
Grant Ammons <gammons@gmail.com>
2014-02-21 17:56:52 -0500

    move to newer version of yo

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('codeApp'));
+  beforeEach(module('bitcoinAngular2App'));
 
   var MainCtrl,
     scope;
