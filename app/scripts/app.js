7ca7307ccc062aeb5835b026b8b04e7f091650e5
Grant Ammons <gammons@gmail.com>
2014-03-01 12:31:47 -0500

    use html5 mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 angular.module('comparator', ['grantDatatables', 'ngRoute', 'ngSanitize','seo'])
   .config(function ($routeProvider, $locationProvider) {
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
